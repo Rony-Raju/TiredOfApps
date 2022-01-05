3160ffc8b03b3769b80e453e459e26cb6da5a2ea
Rony-Raju <Rony.Raju@my.unt.edu>
2022-01-05 01:51:24 -0600

    started with html element manip

--- applier.py
+++ applier.py
@@ -1,8 +1,30 @@
 import os
 from fpdf import FPDF
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
 
-driver = webdriver.Firefox()
+
+
+def indeed():
+    driver = webdriver.Firefox()
+    driver.get('https://www.indeed.com/')
+    login = driver.find_element(By.XPATH, '//div[@class="gnav-header-16i8ao9 eu4oa1w0"]')
+    login.click()
+    email = driver.find_element(By.XPATH, '//input[@id="ifl-InputFormField-3"]')
+    email.send_keys('rony.raju.hsi@gmail.com')
+    submit = driver.find_element(By.XPATH, '//button[@class="css-rhczsh e8ju0x51"]')
+    submit.click()
+    password = driver.find_element(By.XPATH, '//input[@id="ifl-InputFormField-126"]')
+    password.send_keys('')
+    submit = driver.find_element(By.XPATH, '//button[@aria-label="Save job"]')
+    submit.click()
+
+    jobs = driver.find_element(By.XPATH, '//div[@class="icl-u-lg-hide is-embedded"]')
+    jobs.click()
+
+indeed()
+
 
 def letterEdit(company, position):
     pdf = FPDF()
