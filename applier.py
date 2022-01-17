b8ffed683380af6a717916ab7565a1da505eda04
Rony-Raju <Rony.Raju@my.unt.edu>
2022-01-17 02:14:53 -0600

    removed the login function, and just run from a pre-existing profile

--- applier.py
+++ applier.py
@@ -1,24 +1,20 @@
 import os
 from fpdf import FPDF
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver import Firefox
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 vowelList = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u']
 
 
-def indeed():
-    driver = webdriver.Firefox()
+def indeed(options):
+    driver = Firefox(options=options)
     driver.get('https://www.indeed.com/')
-    login = driver.find_element(By.XPATH, '//div[@class="gnav-header-16i8ao9 eu4oa1w0"]')
-    login.click()
-    email = driver.find_element(By.XPATH, '//input[@id="ifl-InputFormField-3"]')
-    email.send_keys('rony.raju.hsi@gmail.com')
-    submit = driver.find_element(By.XPATH, '//button[@class="css-rhczsh e8ju0x51"]')
-    submit.click()
-    password = driver.find_element(By.XPATH, '//input[@id="ifl-InputFormField-126"]')
-    password.send_keys('')
+    #find the user icon and click on saved jobs
+    driver.find_element(By.XPATH,'//div[@class="gnav-header-1dy22ep e37uo190"]').click()
+    driver.find_element(By.XPATH,'//a[@data-gnav-element-name="MyJobs"]').click()
     driver.find_element(By.CSS_SELECTOR, 'button[type=submit]').click()
 
     postings = driver.find_elements(By.CSS_SELECTOR, 'a.atw-ApplyButton')
@@ -27,96 +23,96 @@ def indeed():
     for x in applications:
         driver.get(x)
         driver.find_element(By.XPATH, '//button[@id="indeedApplyButton"]').click()
-        while(#not the last screen):
-            #get the header each time and behave accordingly
-            header = driver.find_element(By.XPATH, '//h1[@class="ia-BasePage-heading fs-unmask"]')
-            header = header.get_attribute('innerHTML')
-            #for preliminary questions
-            if "Questions" in header:
-                questions = driver.find_elements(By.XPATH, '//div[@class="ia-Questions-item css-e9ld6l eu4oa1w0"]')
-                questions_text = [x.get_attribute('innerHTML') for x in questions]
+        header = ''
+        while("Please Review" not in header):
+            try:
+                #get the header each time and behave accordingly
+                header = driver.find_element(By.XPATH, '//h1[@class="ia-BasePage-heading fs-unmask"]')
+                header = header.get_attribute('innerHTML')
+                #for preliminary questions
+                if "Questions" in header:
+                    questions = driver.find_elements(By.XPATH, '//div[@class="ia-Questions-item css-e9ld6l eu4oa1w0"]')
+                    questions_text = [x.get_attribute('innerHTML') for x in questions]
 
-                for i, x in enumerate(questions_text):
-                    if "sponsorship" or "sponsor" in x:
-                        driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//input[@value="0"]'))
-                    elif "education" in x:
-                        driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//input[@value="1301880"]'))
-                    elif "experience" in x:
-                        driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//[@value="1301885"]'))
-                    elif "salary" in x:
-                        questions[i].find_element(By.CSS_SELECTOR, "div.css-d8iwdi e1jgz0i3").send_keys('65000 - 85000')
-                    else:
-                        #weird thing to click, because element obscures
-                        driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//input[@value="1"]'))
-            #next page
-            driver.find_element(By.CSS_SELECTOR, "button.ia-continueButton").click()
-            #for when they ask for a resume
-            if "resume" in header:
-                #next page
-                driver.find_element(By.CSS_SELECTOR, "button.ia-continueButton").click()
-                continue
+                    for i, x in enumerate(questions_text):
+                        if "sponsorship" or "sponsor" in x:
+                            driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//input[@value="0"]'))
+                        elif "education" in x:
+                            driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//input[@value="1301880"]'))
+                        elif "experience" in x:
+                            driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//[@value="1301885"]'))
+                        elif "salary" in x:
+                            questions[i].find_element(By.CSS_SELECTOR, "div.css-d8iwdi e1jgz0i3").send_keys('65000 - 85000')
+                        else:
+                            #weird thing to click, because element obscures
+                            driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//input[@value="1"]'))
+                    #next page
+                    driver.find_element(By.CSS_SELECTOR, "button.ia-continueButton").click()
+                    continue
 
-            #for when they ask for past job experiences
-            print(header)
-            if "past job" in header:
-                #next page
-                driver.find_element(By.CSS_SELECTOR, "button.ia-continueButton").click()
-                continue
-
-            #for the letter
-            if "Want to" in header:
-                #tricky part is the letter
-                job = driver.find_element(By.XPATH, '//div[@class="ia-JobHeader-details"]')
-                job = job.get_attribute('innerHTML')
-                #name of company
-                company = job[:job.find(" - ")-1]
-                print(company)
-                job = driver.find_element(By.XPATH, '//div[@class="ia-JobHeader-title ia-JobHeader-title--withJobDetails"]')
-                job = job.get_attribute('innerHTML')
-                print(job)
-                print(job[0])
-                #correct grammar
-                if job[0] in vowelList:
-                    job = 'an ' + job
+                #for the letter
+                elif "Want to" in header:
+                    #tricky part is the letter
+                    job = driver.find_element(By.XPATH, '//div[@class="ia-JobHeader-details"]')
+                    job = job.get_attribute('innerHTML')
+                    #name of company
+                    company = job[:job.find(" - ")-1]
+                    print(company)
+                    job = driver.find_element(By.XPATH, '//div[@class="ia-JobHeader-title ia-JobHeader-title--withJobDetails"]')
+                    job = job.get_attribute('innerHTML')
                     print(job)
+                    print(job[0])
+                    #correct grammar
+                    if job[0] in vowelList:
+                        job = 'an ' + job
+                        print(job)
+                    else:
+                        job = 'a ' + job
+                    #find the location of the coverletter field and click
+                    driver.find_element(By.XPATH, '//div[@id="write-cover-letter-selection-card"]').click()
+
+                    #don't forget to clear the text field of previous response
+                    driver.find_element(By.XPATH, '//button[@class="ia-Coverletter-edit css-4493ey e8ju0x51"]').click()
+                    #confirmation
+                    driver.find_element(By.XPATH, '//button[@class="ia-ConfirmModal-button-action css-usjv6p e8ju0x51"]').click()
+                    #find location of the text field
+                    coverletter = driver.find_element(By.XPATH, '//textarea[@id="coverletter-textarea"]')
+                    coverletter.click()
+
+                    print(job)
+                    #open the coverletter
+                    print(os.getcwd())
+                    file = open('TiredOfApps/coverletter.txt', 'r')
+
+                    #put these two entries into the coverletter
+                    for i, x in enumerate(file):
+                        if i == 1:
+                            index = x.find('this company')
+                            first = x[0:index]
+                            second = x[index+12:]
+                            x = first + company + ' as ' + job + second
+                        coverletter.send_keys(x)
+                    #sweet this works great
+                    #next page
+                    driver.find_element(By.CSS_SELECTOR, "button.ia-continueButton").click()
+                    continue
+                #finally check if the header is the final confirmation, and submit
+
+                #if the header asks for resume, confirmations, past jobs, or otherwise by default
                 else:
-                    job = 'a ' + job
-                #find the location of the coverletter field and click
-                driver.find_element(By.XPATH, '//div[@id="write-cover-letter-selection-card"]').click()
-
-                #don't forget to clear the text field of previous response
-                driver.find_element(By.XPATH, '//button[@class="ia-Coverletter-edit css-4493ey e8ju0x51"]').click()
-                #confirmation
-                driver.find_element(By.XPATH, '//button[@class="ia-ConfirmModal-button-action css-usjv6p e8ju0x51"]').click()
-                #find location of the text field
-                coverletter = driver.find_element(By.XPATH, '//textarea[@id="coverletter-textarea"]')
-                coverletter.click()
-
-                print(job)
-                #open the coverletter
-                print(os.getcwd())
-                file = open('TiredOfApps/coverletter.txt', 'r')
-
-                #put these two entries into the coverletter
-                for i, x in enumerate(file):
-                    if i == 1:
-                        index = x.find('this company')
-                        first = x[0:index]
-                        second = x[index+12:]
-                        x = first + company + ' as ' + job + second
-                    coverletter.send_keys(x)
-                #sweet this works great
-                #next page
-                driver.find_element(By.CSS_SELECTOR, "button.ia-continueButton").click()
-                continue
-            #finally check if the header is the final confirmation, and submit
-            if "Please review" in header:
-                driver.find_element(By.CSS_SELECTOR, "button.ia-continueButton").click()
-                continue
+                    driver.find_element(By.CSS_SELECTOR, "button.ia-continueButton").click()
+                    continue
+            except ValueError:
+                print("Couldn't find value")
+                f = open('TiredOfApps/unfinishedApps.txt', w)
+                f.write("unfinished app: ", x)
+                f.close
+                break
 
 
 
-indeed()
+
+
 
 
 def letterEdit(company, position):
@@ -139,10 +135,10 @@ def letterEdit(company, position):
 #     "references" : ""
 # }
 
+options = Options()
+profile_path = r'C:\Users\Rony\AppData\Roaming\Mozilla\Firefox\Profiles\ag1yjdfi.SeleniumProfile'
+options.add_argument("-profile=C:\\Users\\Rony\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\ag1yjdfi.SeleniumProfile")
 
-dir = 'F:\\Dropbox\\UNT'
-items = list(os.listdir(dir))
-for i in items:
-    print(i)
 
-letterEdit("Bimbleton Corp", "developer")
+
+indeed(options)
