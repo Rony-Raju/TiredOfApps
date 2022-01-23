37f2a1dcd8a236214d9b4dc61dbd85d06377ed93
Rony-Raju <Rony.Raju@my.unt.edu>
2022-01-23 00:44:17 -0600

    "managed to get it to follow predetermined profile, but still more problems"

--- applier.py
+++ applier.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from fpdf import FPDF
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Firefox
@@ -8,6 +9,12 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 vowelList = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u']
 
+def next(driver):
+    buttons = driver.find_elements(By.tag_name, "button")
+    for text in buttons:
+        if 'continue' in text.get_attribute('innerHTML'):
+            text.click()
+            return
 
 def indeed(options):
     driver = Firefox(options=options)
@@ -15,7 +22,6 @@ def indeed(options):
     #find the user icon and click on saved jobs
     driver.find_element(By.XPATH,'//div[@class="gnav-header-1dy22ep e37uo190"]').click()
     driver.find_element(By.XPATH,'//a[@data-gnav-element-name="MyJobs"]').click()
-    driver.find_element(By.CSS_SELECTOR, 'button[type=submit]').click()
 
     postings = driver.find_elements(By.CSS_SELECTOR, 'a.atw-ApplyButton')
 
@@ -24,6 +30,7 @@ def indeed(options):
         driver.get(x)
         driver.find_element(By.XPATH, '//button[@id="indeedApplyButton"]').click()
         header = ''
+        link = x
         while("Please Review" not in header):
             try:
                 #get the header each time and behave accordingly
@@ -34,20 +41,38 @@ def indeed(options):
                     questions = driver.find_elements(By.XPATH, '//div[@class="ia-Questions-item css-e9ld6l eu4oa1w0"]')
                     questions_text = [x.get_attribute('innerHTML') for x in questions]
 
-                    for i, x in enumerate(questions_text):
-                        if "sponsorship" or "sponsor" in x:
-                            driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//input[@value="0"]'))
-                        elif "education" in x:
-                            driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//input[@value="1301880"]'))
-                        elif "experience" in x:
-                            driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//[@value="1301885"]'))
-                        elif "salary" in x:
+                    for i, text in enumerate(questions_text):
+                        try:
+                            #next page
+                            next(driver)
+                            continue
+                        except Exception:
+                            pass
+                        if "sponsorship" in text or "sponsor" in text:
+                            try:
+                                driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//input[@value="0"]'))
+                            except Exception:
+                                pass
+                        elif "education" in text:
+                            driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//input[@value="Bachelor\'s"]'))
+                        elif "experience" in text:
+                            #find the selection and select it
+                            try:
+                                answers = text.find_elements(By.CSS_SELECTOR, 'label.css-d0lawn es2vvo70')
+                                for selection in answers:
+                                    choice = selection.find_element(By.CSS_SELECTOR, "span.css-19kaor0 eu4oa1w0")
+                                    if '3' in choice.get_attribute('innerHTML'):
+                                        selection.click()
+                                        break
+                            except Exception:
+                                text.find_element(By.CSS_SELECTOR, 'input.css-qxtdct e1jgz0i3').send_keys('3')
+                        elif "salary" in text:
                             questions[i].find_element(By.CSS_SELECTOR, "div.css-d8iwdi e1jgz0i3").send_keys('65000 - 85000')
                         else:
                             #weird thing to click, because element obscures
                             driver.execute_script("arguments[0].click()", questions[i].find_element(By.XPATH, '//input[@value="1"]'))
                     #next page
-                    driver.find_element(By.CSS_SELECTOR, "button.ia-continueButton").click()
+                    next(driver)
                     continue
 
                 #for the letter
@@ -69,15 +94,11 @@ def indeed(options):
                     else:
                         job = 'a ' + job
                     #find the location of the coverletter field and click
-                    driver.find_element(By.XPATH, '//div[@id="write-cover-letter-selection-card"]').click()
-
-                    #don't forget to clear the text field of previous response
-                    driver.find_element(By.XPATH, '//button[@class="ia-Coverletter-edit css-4493ey e8ju0x51"]').click()
-                    #confirmation
-                    driver.find_element(By.XPATH, '//button[@class="ia-ConfirmModal-button-action css-usjv6p e8ju0x51"]').click()
-                    #find location of the text field
-                    coverletter = driver.find_element(By.XPATH, '//textarea[@id="coverletter-textarea"]')
+                    coverletter = driver.find_element(By.CSS_SELECTOR, "textarea.ia-Coverletter-textarea")
                     coverletter.click()
+                    #don't forget to clear the text field of previous response
+                    coverletter.send_keys(Keys.CONTROL+'a')
+                    coverletter.send_keys(Keys.BACK_SPACE)
 
                     print(job)
                     #open the coverletter
@@ -94,24 +115,30 @@ def indeed(options):
                         coverletter.send_keys(x)
                     #sweet this works great
                     #next page
-                    driver.find_element(By.CSS_SELECTOR, "button.ia-continueButton").click()
+                    next(driver)
                     continue
                 #finally check if the header is the final confirmation, and submit
 
                 #if the header asks for resume, confirmations, past jobs, or otherwise by default
                 else:
-                    driver.find_element(By.CSS_SELECTOR, "button.ia-continueButton").click()
+                    #continued weirdness. Now just look for buttons with innerHTML with 'continue'
+                    buttons = driver.find_elements(By.tag_name, "button")
+                    for text in buttons:
+                        if 'continue' in text.get_attribute('innerHTML'):
+                            text.click()
                     continue
-            except ValueError:
-                print("Couldn't find value")
-                f = open('TiredOfApps/unfinishedApps.txt', w)
-                f.write("unfinished app: ", x)
-                f.close
+            except Exception:
+                print("Something didn't work")
+                f = open('TiredOfApps/unfinishedApps.txt', 'a+')
+                now = datetime.now()
+                current_time = now.strftime("%m-%d-%Y %H:%M:%S")
+                f.write(f"{current_time} unfinished app: {link}\n{Exception.with_traceback}\n\n")
+                f.close()
                 break
+        driver.find_element(By.CSS_SELECTOR, "button.ia-continueButton").click()
 
 
     driver.close()
-
 
 
 
@@ -136,9 +163,13 @@ def letterEdit(company, position):
 # }
 
 options = Options()
-profile_path = r'C:\Users\Rony\AppData\Roaming\Mozilla\Firefox\Profiles\ag1yjdfi.SeleniumProfile'
-options.add_argument("-profile=C:\\Users\\Rony\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\ag1yjdfi.SeleniumProfile")
+options.add_argument("-profile ed53ej1d.Gecko")
+options.set_preference("remote.prefs.recommended", False)
+options.set_preference("dom.allow_scripts_to_close_windows", True)
+options.set_preference("browser.tabs.closeWindowWithLastTab", True)
+options.add_argument("--marionette-port 42069")
 
 
-
+# cwd = os.getcwd()
+# print(cwd)
 indeed(options)
